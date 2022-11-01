2cea930af966fdabeb9ad6314b2ad9570ae373c8
leonelfotsing <leonelfotsing95@gmail.com>
2022-11-01 11:02:48 +0100

    ajout du filtre sur les status

--- app/glados/repositories/entities.py
+++ app/glados/repositories/entities.py
@@ -4,8 +4,20 @@ from glados.models import Entity
 def get_entities(filters):
     query = Entity.query
 
+    #obtain all filters
     type = filters.get("type")
+    status = filters.get("status")
+    room_id = filters.get("room")
+
+    #get all entities by avalaibles filters
     if type:
         query = query.filter(Entity.type == type)
 
+    if status:
+        query = query.filter(Entity.status == status)
+
+    if room_id:
+        query = query.filter(Entity.room_id == room_id)
+
     return query
+
